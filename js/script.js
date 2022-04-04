04204cda282b5b5d2c4c72abaf56ccd993f5cf98
Aaron <zhangchuqi1998@gmail.com>
2022-04-04 14:38:20 -0500

    about

--- js/script.js
+++ js/script.js
@@ -1,64 +1,77 @@
-// Dark theme
-const themeButton = document.getElementById("theme-button");
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
+//===================================== DARK THEME =========================
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
 
-const selectedTheo = localStorage.getItem('selected-theme');
+// PREVEIOSLY SELECTED TOPIC (checking from local storage)
+const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light';
-const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun';
+// We obtain the current theme that the interface has by validating the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
 
-if (selectedTheo) {
-    document.body.classList[selectedTheo === 'dark'?'add':'remove'](darkTheme);
-    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme);
+//We need to validate if the user has previously chosen a topic
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme)
 }
 
+// Activate/ deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
+    //ADD or remove the dark/light icon -- icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
+    //We save the theme and the current icon that the user has chosen
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
-console.log("it is working")
+console.log("THEME SETTING IS WORKING!")
 
-// menu show y hidden
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+//===================================== MENU SHOW Y HIDDEN =========================
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
-
-if (navToggle) {
-    navToggle.addEventListener('click', ()=> {
-        navMenu.classList.add('show_menu')
+// ================  MENU SHOW  =============
+/*  Validate if the constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
     })
-}
+} 
 
-if (navClose) {
-    navClose.addEventListener('click', ()=> {
-        navMenu.classList.remove('show_menu')
+// ================  MENU HIDE  =============
+/*  Validate if the constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
     })
-}
+} 
 
 
-console.log("menu working")
 
-const navLink = document.querySelectorAll('.nav-link')
+console.log("MENU Y SETTING WORKING!")
+//===================================== REMOVE MENU PRORFILE =========================
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show_menu')
+    // when we click on nav__links, we remove the show menu
+    navMenu.classList.remove('show-menu')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 console.log("Remove menu profile is working!")
 
 
-/* Typewriter */
+//===================================== Typewriter Effect =========================
+
 
 new Typewriter('#typewriter', {
-  strings: ['Chuqi Zhang', 'Software Develper'],
+  strings: ['Biplov Jha', 'Web-developer', 'Electrical-Engineer', 'e-Tutor', 'Author', ''],
   autoStart: true,
   loop: true,
   cursor: "|"
 });
+console.log("Typewriter effect is working!")
